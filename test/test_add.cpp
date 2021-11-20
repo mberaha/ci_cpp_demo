9d2df070b20ba225cf901cd9d307bb3e0fb1f2d9
Mario beraha <berahamario@gmail.com>
2021-11-20 09:07:42 +0100

    failing test

--- test/test_add.cpp
+++ test/test_add.cpp
@@ -10,3 +10,9 @@ TEST(add, right) {
  ASSERT_EQ(10, add(5, 5));
  ASSERT_EQ(0, add(0, 0));
 }
+
+TEST(add, wrong) {
+ ASSERT_EQ(2, add_wrong(1, 1));
+ ASSERT_EQ(3, add_wrong(1, 2));
+ ASSERT_EQ(3, add_wrong(2, 1));
+}
